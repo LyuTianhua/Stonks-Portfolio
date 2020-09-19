5eb2c64e0f7f7a069d6a97727dfc38237be9ec4f
hern784@usc.edu <hern784@usc.edu>
2020-09-18 23:16:29 -0700

    added validate() shell function and testValidate()

--- src/test/java/junit/LoginTest.java
+++ src/test/java/junit/LoginTest.java
@@ -5,19 +5,22 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LoginTest {
 
-    Connection con;
+    private static Connection con;
 
     @BeforeClass
     public static void setUp() throws Exception {
         /*
         Todo: init connection to database
         * */
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CSCI_310_PROJECT?user=root&password=MysqlPass11!!&serverTimezone=PST");
     }
 
     @Test
@@ -42,12 +45,10 @@ public class LoginTest {
     @Test
     public void testValidate() {
 
-        Connection connection = con;
+        boolean correct = Login.validate(con, "tu1", "tu1pass");
+        boolean incorrect = Login.validate(con, "tu1", "wrongPass");
 
-//        boolean correct = Login.validate(connection, "tu1", "tu1pass");
-//        boolean incorrect = Login.validate(connection, "tu1", "wrongPass");
-
-        assertTrue(true);
+        assertTrue(correct && !incorrect);
     }
 
 }
