d5d683bb454a9cd7fe29afa47f90de79e328a59b
Lyu Tianhua <tianhual@usc.edu>
2020-10-07 22:00:16 -0700

    Feature timeout inactive (#64)
    * Inactivity
    
    * Test completed. Ready to merge.
    
    Co-authored-by: John <jjmarsha@usc.edu>

--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -207,6 +207,26 @@
 		document.querySelector("#logout").onclick = function(event) {
 			window.location.href = "index.jsp";
 		}
+                                        
+		function idleTimer() {
+			let t;
+			//window.onload = resetTimer;
+			window.onmousemove = resetTimer; // catches mouse movements
+			window.onmousedown = resetTimer; // catches mouse movements
+			window.onclick = resetTimer;     // catches mouse clicks
+			window.onscroll = resetTimer;    // catches scrolling
+			window.onkeypress = resetTimer;  //catches keyboard actions
+
+			function logout() {
+				window.location.href = '/index.jsp';  //Adapt to actual logout script
+			}
+
+			function resetTimer() {
+				clearTimeout(t);
+				t = setTimeout(logout, 3000);  // time is in milliseconds (1000 is 1 second)
+			}
+		}
+		idleTimer();
 	</script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
---
+++ src/test/java/cucumber/inactiveStepDefinition.java
@@ -0,0 +1,63 @@
+package cucumber;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class inactiveStepDefinition {
+	private static final String ROOT_URL = new Configurations().url;
+
+	private final WebDriver driver = new ChromeDriver();
+
+	@Given("I am on the home page")
+	public void i_am_on_the_home_page() {
+		driver.get(ROOT_URL + "home.jsp");
+	}
+	
+	@When("I do nothing for 5 seconds")
+	public void iDoNothingForSeconds() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@When("I click title")
+	public void iClickTitle() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.findElement(By.linkText("USC CS 310 Stock Portfolio Management")).click();
+	}
+	
+	@Then("I should be at page {string}")
+	public void iShouldBeAtPage(String url) {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertTrue(driver.getCurrentUrl().contains(url));
+	}
+	
+	@After()
+	public void cleanup() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+}
