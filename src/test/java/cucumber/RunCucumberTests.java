5c41074160ef0075565c083a1e11cecb437750e2
John <jjmarsha@usc.edu>
2020-10-22 20:27:53 -0700

    Finished 3 failed login attempts

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -13,8 +13,8 @@ import java.util.logging.Level;
  * Run all the cucumber tests in the current package.
  */
 @RunWith(Cucumber.class)
-//@CucumberOptions()
-@CucumberOptions(features = {"src/test/resources/cucumber/failedAttempts.feature"})
+@CucumberOptions()
+//@CucumberOptions(features = {"src/test/resources/cucumber/failedAttempts.feature"})
 
 public class RunCucumberTests {
 
