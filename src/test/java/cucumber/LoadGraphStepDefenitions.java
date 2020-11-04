122ecebb632fcef91368d5596a85840b59202df1
Dariya Khojasteh <dkhojast@usc.edu>
2020-11-03 23:03:20 -0800

    Added error messages for the graph and acceptance tests for them.

--- src/test/java/cucumber/LoadGraphStepDefenitions.java
+++ src/test/java/cucumber/LoadGraphStepDefenitions.java
@@ -3,7 +3,9 @@ package cucumber;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -44,4 +46,101 @@ public class LoadGraphStepDefenitions {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("myChart")));
         assertTrue(driver.findElement(By.id("myChart")).isDisplayed());
     }
+    
+    @Then("I should see an error message stating that these are invalid dates below the graph")
+    public void i_should_see_an_error_message_stating_that_these_are_invalid_dates_below() {
+        try {
+            Thread.sleep(2000);
+        } catch(Exception ie) {
+        }
+
+        Boolean checkIfElementPresent= false;
+        //Checks if the invalid dates error message is present
+        if(driver.findElements(By.xpath("//*[@id=\"invalid-dates\"]")).size()!= 0) {
+            checkIfElementPresent = true;
+        }
+        assertTrue(checkIfElementPresent);
+    }
+    
+    @And("I select 1 week ago from the calendar picker for date from")
+    public void select_one_week_ago_calendar_for_from() {
+    	try {
+			Thread.sleep(2000);
+		} catch(Exception ie) {
+			//System.out.println("Exception in invalid 1 year date test.");
+		}
+    	
+    	WebElement purchaseDate = driver.findElement(By.xpath("//*[@id=\"fromGraph\"]"));
+    	purchaseDate.click();
+    	purchaseDate.sendKeys(Keys.TAB);
+    	purchaseDate.sendKeys(Keys.TAB);
+    	purchaseDate.sendKeys(Keys.TAB);
+    	purchaseDate.sendKeys(Keys.ENTER);
+    	purchaseDate.sendKeys(Keys.ARROW_UP);
+    	purchaseDate.sendKeys(Keys.ENTER);
+    }
+    
+    @Then("date from should equal 1 week ago")
+    public void date_should_equal_one_week_ago_for_from() {
+    	try {
+			Thread.sleep(2000);
+		} catch(Exception ie) {
+		}
+    	
+    	WebElement purchaseDate = driver.findElement(By.xpath("//*[@id=\"fromGraph\"]"));
+    	assertTrue(!purchaseDate.getAttribute("value").isEmpty());
+    }
+    
+    @And("I select 1 week ago from the calendar picker for date to")
+    public void select_one_week_ago_calendar_for_to() {
+    	try {
+			Thread.sleep(2000);
+		} catch(Exception ie) {
+			//System.out.println("Exception in invalid 1 year date test.");
+		}
+    	
+    	WebElement purchaseDate = driver.findElement(By.xpath("//*[@id=\"toGraph\"]"));
+    	purchaseDate.click();
+    	purchaseDate.sendKeys(Keys.TAB);
+    	purchaseDate.sendKeys(Keys.TAB);
+    	purchaseDate.sendKeys(Keys.TAB);
+    	purchaseDate.sendKeys(Keys.ENTER);
+    	purchaseDate.sendKeys(Keys.ARROW_UP);
+    	purchaseDate.sendKeys(Keys.ENTER);
+    }
+    
+    @Then("date to should equal 1 week ago")
+    public void date_should_equal_one_week_ago_for_to() {
+    	try {
+			Thread.sleep(2000);
+		} catch(Exception ie) {
+		}
+    	
+    	WebElement purchaseDate = driver.findElement(By.xpath("//*[@id=\"toGraph\"]"));
+    	assertTrue(!purchaseDate.getAttribute("value").isEmpty());
+    }
+    
+    @And("I see my portfolio value")
+    public void i_see_my_portfolio_value() {
+    	try {
+			Thread.sleep(2000);
+		} catch(Exception ie) {
+		}
+    	
+    	WebElement portfolioValue = driver.findElement(By.xpath("//*[@id=\"portfolio-value-number\"]"));
+    	assertTrue(!portfolioValue.getAttribute("value").isEmpty());
+    }
+    
+    @Then("the portfolio value should be green or red with an up or down arrow by it")
+    public void the_portfolio_value_should_be() {
+    	try {
+			Thread.sleep(2000);
+		} catch(Exception ie) {
+		}
+    	
+    	WebElement portfolioValue = driver.findElement(By.xpath("//*[@id=\"portfolio-value-number\"]"));
+    	assertTrue(!portfolioValue.getCssValue("color").equals("black"));
+    }
+
+    
 }
--- src/test/resources/cucumber/LoadGraph.feature
+++ src/test/resources/cucumber/LoadGraph.feature
@@ -8,3 +8,49 @@ Feature: Graph displayed on home page
     Given I am signed in
     And I have added stock
     Then I should see graphed stock data
+  
+  Scenario Outline: Adding date from after date to
+  	Given I am signed in
+  	And I enter <from> into 'fromGraph'
+    And I enter <to> into 'toGraph'
+    Then I should see an error message stating that these are invalid dates below the graph
+    Examples:
+      | from             | to             |
+      | '01/15/2020'     | '01/14/2020'   |
+      | '08/20/2020'     | '07/28/2020'   |
+  
+  Scenario Outline: Adding date "from" more than 1 year ago
+    Given I am signed in
+    And I enter <from> into 'fromGraph'
+    Then I should see an error message stating that these are invalid dates below the graph
+    Examples:
+      | from             |
+      | '01/15/2019'     |
+      | '08/20/2019'     |
+  
+  Scenario Outline: Adding date "to" in the future
+    Given I am signed in
+    And I enter <to> into 'toGraph'
+    Then I should see an error message stating that these are invalid dates below the graph
+    Examples:
+      | to               |
+      | '01/15/2021'     |
+      | '08/20/2021'     |
+      
+  Scenario: Using calendar picker to select dates in date from
+      Given I am signed in
+      And I select 1 week ago from the calendar picker for date from
+      Then date from should equal 1 week ago
+      
+  Scenario: Using calendar picker to select dates in date to
+      Given I am signed in
+      And I select 1 week ago from the calendar picker for date to
+      Then date to should equal 1 week ago
+      
+  Scenario: Portfolio value is red or green with an arrow signaling gains or losses
+  	Given I am signed in
+  	And I see my portfolio value
+  	Then the portfolio value should be green or red with an up or down arrow by it
+  	
+  
+  
